ea1884ee5e64a4627b8c7dd7f24415da8a3e2b39
= <=>
2025-06-24 16:19:08 -0300

    primeiro

--- src/App.jsx
+++ src/App.jsx
@@ -27,13 +27,14 @@ function App() {
   ]);
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
-      <div className="w-[500px]" > 
-      <h1 className="text-3xl text-slate-100 font-bold text-center">
-        Gerenciador de Tarefas
-      </h1>
+      <div className="w-[500px]">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          Gerenciador de Tarefas
+        </h1>
 
-      <Tasks tasks={tarefa} />
-    </div>
+        <Tasks tasks={tarefa} />
+        <button onClick={() => console.log(setTarefa)}></button>
+      </div>
     </div>
   );
 }
